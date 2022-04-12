2cc501b455bf2038c6040c15fd62336316e3c57e
= <=>
2022-04-12 16:31:51 +0100

    submit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,22 +3,22 @@
 """
 Program that generates pascal triangle based on user input number of rows
 """
-def pascal_triangle(n):
+def pascal_triangle(number_of_rows):
     """
     function that returns pascal's triangle in 2D
     """
     triangle = []
-    if n <= 0:
+    if number_of_rows <= 0:
         pass
-    elif n == 1:
+    elif number_of_rows == 1:
         triangle.append([1])
-    elif n == 2:
+    elif number_of_rows == 2:
         triangle.append([1])
         triangle.append([1, 1])
     else:
         triangle.append([1])
         triangle.append([1, 1])
-        for i in range(2, n):
+        for i in range(2, number_of_rows):
             sub = []
             sub.append(1)
             for j in range(0, i-1):
